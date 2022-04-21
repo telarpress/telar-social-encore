b00673beb419eac9ad8167ad901b54656026caaf
amir <amir@telar.dev>
2022-04-21 11:37:40 +0700

    fix local hello micro

--- hello/common.go
+++ hello/common.go
@@ -9,6 +9,11 @@ import (
 func RemoveBaseURLFromRequest(r *http.Request) {
 	r.URL.Path = RemoveBaseURL(r.URL.Path)
 	r.RequestURI = RemoveBaseURL(r.RequestURI)
+	// If Encore is running in local development the remote address is not a valid hostname,
+	// which Fiber expects. Set this to a valid placeholder value to appease Fiber. 
+    if strings.Contains(r.RemoteAddr, "yamux") {
+		r.RemoteAddr = "localhost:0"
+	}
 }
 
 // RemoveBaseURL removes the base url from the request path
