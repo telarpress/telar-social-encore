e32d8d81000788ca8a264e23c9c4893586b69162
amir <amir@telar.dev>
2023-12-31 19:16:32 +0700

    chore: new config deployment

--- hello/hello.go
+++ hello/hello.go
@@ -12,31 +12,27 @@ import (
 // Cache state
 var app *fiber.App
 
-
-
 // init
 func init() {
-
-
 
 	// Initialize app
 	app = fiber.New()
 	app.Get("/:name", func(c *fiber.Ctx) error {
-		fmt.Println("[ "+os.Getenv("PORT")+" ]")
+		fmt.Println("[ " + os.Getenv("PORT") + " ]")
 		name := c.Params("name")
-        return c.SendString("Hello, World with secrets 👋 !"+name )
-    })
+		return c.SendString("Hello, World 👋 !" + name)
+	})
 }
 
 // This is a simple REST API that responds with a personalized greeting.
 // To call it, run in your terminal:
 //
-//     curl http://localhost:4000/hello/name
+//	curl http://localhost:4000/hello/name
 //
 //encore:api public raw path=/hello/*p1
 func Handle(w http.ResponseWriter, r *http.Request) {
 	RemoveBaseURLFromRequest(r)
-	
+
 	// Call the app
 	adaptor.FiberApp(app)(w, r)
 }
